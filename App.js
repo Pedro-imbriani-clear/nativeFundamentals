8547ca046189e68bb18507fece4f39ab6b3ea0cd
pedro-imbriani-clear <pedro.imbriani.clear@gmail.com>
2023-07-22 15:52:01 -0300

    init

--- App.js
+++ App.js
@@ -1,13 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+class App extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Ola Mundo</Text>
+      </View>
+    )
+  };
 }
 
 const styles = StyleSheet.create({
@@ -18,3 +19,4 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+export default App
