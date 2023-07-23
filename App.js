28a1757165014372be1438f0f7998b3654c05af4
pedro-imbriani-clear <pedro.imbriani.clear@gmail.com>
2023-07-23 16:15:38 -0300

    creating components

--- App.js
+++ App.js
@@ -11,19 +11,33 @@ class App extends Component {
         <Text>Meu primeiro app</Text>
         <Text style={{ color: 'red', fontSize: 25, margin: 12 }}>Seja um programador</Text>
         <Image
-          source={{ uri: 'https://nutrata.com.br/wp-content/uploads/2023/03/Banner-Blog-1-1536x1024.jpg.webp' }}
-          style={{ width: 300, height: 300 }}
+          source={{ uri: 'https://i.pinimg.com/474x/8b/c1/9d/8bc19d5c0a1f0e318cc96798826069f3.jpg' }}
+          style={{ width: 300, height: 300, margin:15 }}
         />
         <Text style={{ fontSize: 30 }}>
           {nome}
         </Text>
         <Image
           source={{ uri: img }}
-          style={{ width: 300, height: 300 }}
+          style={{ width: 300, height: 300, margin:15 }}
         />
+        <Coleman largura={500} altura={200}/>
 
       </View>
     )
   };
 }
+
 export default App
+class Coleman extends Component{
+  render(){
+    let imgs ='https://nutrata.com.br/wp-content/uploads/2023/03/Banner-Blog-1-1536x1024.jpg.webp'
+
+    return(
+      <Image
+      source={{uri: imgs}}
+      fontSize={{width: this.props.largura, height: this.props.altura, margin:15}}
+       />
+    )
+  }
+}
