74285ce750dba1492f1495b0245951eef0ae9b16
pedro-imbriani-clear <pedro.imbriani.clear@gmail.com>
2023-07-23 23:48:17 -0300

    Dynamic and fixed sizes

--- App.js
+++ App.js
@@ -1,27 +1,14 @@
 import { StyleSheet, Text, View } from 'react-native';
 import React, { Component } from 'react';
-
-const style = StyleSheet.create({
-  area: {
-    marginTop: 40
-  },
-  textoPrincipal:{
-    fontSize:40,  
-    color:'#ff0000',
-    textAlign:'center'
-  },
-
-});
 class App extends Component {
 
   render() {
 
     return (
-      <View style={style.area} >
-        <Text style={style.textoPrincipal}>Ola Mundo1</Text>
-        <Text  >Ola Mundo2</Text>
-        <Text >Ola Mundo3</Text>
-        <Text style={[style.textoPrincipal, style.area]} >Ola Mundo4</Text>
+      <View style={{backgroundColor:'#222',flex:1}}>
+        <View style={{height:45, backgroundColor: '#222' }}></View>
+        <View style={{ flex:2, backgroundColor: 'green' }}></View>
+        <View style={{height:45, backgroundColor: '#222' }}></View>
       </View>
     )
   };
