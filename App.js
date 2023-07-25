6c4fbad26bdc3bfaf89638b324e78ff77f2db253
pedro-imbriani-clear <pedro.imbriani.clear@gmail.com>
2023-07-25 00:17:59 -0300

    fortune cookie

--- App.js
+++ App.js
@@ -1,58 +1,98 @@
-import { StyleSheet, Text, View,TextInput,Button } from 'react-native';
 import React, { Component } from 'react';
+import { View, Text, StyleSheet, TouchableOpacity, Image } from 'react-native';
+
 class App extends Component {
-  constructor(props){
-    super(props)
-    this.state= {
-      nome: '',
-      input:''
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      textoFrase: '',
+      img: require('./src/biscoito.png'),
     };
-    this.entrar = this.entrar.bind(this)
+
+    this.quebraBiscoito = this.quebraBiscoito.bind(this);
+
+    this.frases = [
+      'Siga os bons e aprenda com eles.',
+      'O bom-senso vale mais do que muito conhecimento.',
+      'O riso é a menor distância entre duas pessoas.',
+      'Deixe de lado as preocupações e seja feliz.',
+      'Realize o óbvio, pense no improvável e conquiste o impossível.',
+      'Acredite em milagres, mas não dependa deles.',
+      'A maior barreira para o sucesso é o medo do fracasso.'
+    ];
+
   }
 
-  entrar(){
-    if(this.state.input === ''){
-      alert('Digite seu nome')
-      return;
-    }
-    this.setState({nome: 'Bem vindo ' + this.state.input})
+  quebraBiscoito() {
+    let numeroAleatorio = Math.floor(Math.random() * this.frases.length);
+
+    this.setState({
+      textoFrase: ' "' + this.frases[numeroAleatorio] + '" ',
+      img: require('./src/biscoitoAberto.png')
+    });
 
   }
+
+
   render() {
-
     return (
-      <View style={styles.container}>
-        <TextInput style={styles.input} 
-          placeholder='Digite seu nome'
-          underlineColorAndroid='transparent'
-          onChangeText={(texto) => this.setState({input: texto})}
-          />
-        <Button title='Entrar' onPress={this.entrar}/>
-        <Text style={styles.text}>
-         {this.state.nome}
-        </Text>
+      <View style={styles.container} >
+
+        <Image
+          source={this.state.img}
+          style={styles.img}
+        />
+
+        <Text style={styles.textoFrase}>{this.state.textoFrase}</Text>
+
+        <TouchableOpacity style={styles.botao} onPress={this.quebraBiscoito}>
+          <View style={styles.btnArea}>
+            <Text style={styles.btnTexto}>Quebrar Biscoito</Text>
+          </View>
+        </TouchableOpacity>
+
       </View>
-    )
-  };
+    );
+  }
+
 }
 
 const styles = StyleSheet.create({
-  container:{
-    flex:1,
-    marginTop:15
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center'
   },
-  input:{
-    height:45,
-    borderWidth:1,
-    borderColor:'#222',
-    margin:10,
-    fontSize:20,
-    padding:10
+  img: {
+    width: 250,
+    height: 250,
   },
-  text:{
-    textAlign:'center',
-    fontSize:25
+  textoFrase: {
+    fontSize: 20,
+    color: '#dd7b22',
+    margin: 30,
+    fontStyle: 'italic',
+    textAlign: 'center'
+  },
+  botao: {
+    width: 230,
+    height: 50,
+    borderWidth: 2,
+    borderColor: '#dd7b22',
+    borderRadius: 25
+  },
+  btnArea: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  btnTexto: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: '#dd7b22'
   }
-})
+});
 
-export default App
+export default App;
